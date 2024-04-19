961387794469a729a934227fc7c3fc5ed7bc1ab6
Madd-G <akhmadnuralamsyah@gmail.com>
2024-04-19 07:35:04 +0700

    feat(view-user-detail): implement user detail view feature
    Add functionality to view detailed information about a user. Users can now access a dedicated  modal to view comprehensive details of a selected user, including their name, email, address, and other relevant information. Key features include:
    
    - UI integration: Implement user interface components to display user details in a clear and organized manner.
    - Fetch user data: Retrieve user details to show the detail view.
    - Navigation: Enable seamless navigation between the user list and user detail views for a smooth user experience.

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,28 +4,33 @@ class AppColors {
   const AppColors._();
 
   /// #6192DA
-  static const primaryColor = Color(0xFF6192DA);
+  static const Color primaryColor = Color(0xFF6192DA);
 
   /// #E5EEF9
-  static const secondaryColor = Color(0xFFE5EEF9);
+  static const Color secondaryColor = Color(0xFFE5EEF9);
 
   /// #F7F6FC
-  static const backgroundColor = Color(0xFFF7F6FD);
+  static const Color backgroundColor = Color(0xFFF7F6FD);
 
   /// BLACK
-  static const blackColor = Color(0xFF000000);
+  static const Color blackColor = Color(0xFF000000);
 
   /// WHITE
-  static const whiteColor = Color(0xFFFFFFFF);
+  static const Color whiteColor = Color(0xFFFFFFFF);
 
   /// GREY
-  static const greyColor = Color(0xFF5F6266);
+  static const Color greyColor = Color(0xFF5F6266);
 
   /// GREEN
-  static const greenColor = Color(0xFF69C9AB);
-  static const greenSecondaryColor = Color(0xFFE4F6F1);
+  static const Color greenColor = Color(0xFF69C9AB);
+  static const Color greenSecondaryColor = Color(0xFFE4F6F1);
 
   /// RED
-  static const redColor = Color(0xFFE791B7);
-  static const redSecondaryColor = Color(0xFFF7DFE9);
+  static const Color redColor = Color(0xFFE791B7);
+  static const Color redSecondaryColor = Color(0xFFF7DFE9);
+
+  /// BORDER
+  static const Color borderPrimary = Color(0xFFE2E8F0);
+  static const Color borderSecondary = Color(0xFFCBD5E0);
+
 }
